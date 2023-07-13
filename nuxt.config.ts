32a4bd4a8cc0a4315caa3ffb9e95ef58d66c98ba
Lex <lexrex618@gmail.com>
2023-07-13 22:08:55 +0300

    Add ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase'],
+  modules: [
+    '@nuxtjs/supabase',
+    '@nuxthq/ui'
+  ],
   css: ['@/assets/main.css'],
 })
