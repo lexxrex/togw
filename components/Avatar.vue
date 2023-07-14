d246fb3e9af7781beb6bf33ea135b16f68187407
Lex <lexrex618@gmail.com>
2023-07-14 10:13:05 +0300

    Edit Avatar component

--- components/Avatar.vue
+++ components/Avatar.vue
@@ -2,14 +2,8 @@
 // eslint-disable-next-line vue/require-prop-types
 const props = defineProps(['path'])
 const { path } = toRefs(props)
-
-const emit = defineEmits(['update:path', 'upload'])
-
 const supabase = useSupabaseClient()
-
-const uploading = ref(false)
 const src = ref('')
-const files = ref()
 
 const downloadImage = async () => {
   try {
@@ -17,58 +11,25 @@ const downloadImage = async () => {
     if (error) { throw error }
     src.value = URL.createObjectURL(data)
   } catch (error) {
-    console.error('Error downloading image: ', error.message)
-  }
-}
-
-const uploadAvatar = async (evt) => {
-  files.value = evt.target.files
-  try {
-    uploading.value = true
-
-    if (!files.value || files.value.length === 0) {
-      throw new Error('You must select an image to upload.')
-    }
-
-    const file = files.value[0]
-    const fileExt = file.name.split('.').pop()
-    const fileName = `${Math.random()}.${fileExt}`
-    const filePath = `${fileName}`
-
-    const { error: uploadError } = await supabase.storage.from('avatars').upload(filePath, file)
-
-    if (uploadError) { throw uploadError }
-
-    emit('update:path', filePath)
-    emit('upload')
-  } catch (error) {
-    alert(error.message)
-  } finally {
-    uploading.value = false
+    console.error('Error downlrounded-full: ', error.message)
   }
 }
 
 downloadImage()
 
-watch(path, () => {
-  if (path.value) {
-    downloadImage()
-  }
-})
 </script>
 
 <template>
-  <div>
+  <div class="w-20 h-20">
     <img
       v-if="src"
       :src="src"
       alt="Avatar"
-      class="avatar image"
-      style="width: 10em; height: 10em;"
+      class="rounded-full"
     >
-    <div v-else class="avatar no-image" :style="{ height: size, width: size }" />
+    <div v-else class="rounded-full w-max h-max" />
 
-    <div style="width: 10em; position: relative;">
+    <!-- <div style="width: 10em; position: relative;">
       <label class="button primary block" for="single">
         {{ uploading ? 'Uploading ...' : 'Upload' }}
       </label>
@@ -76,10 +37,10 @@ watch(path, () => {
         id="single"
         style="position: absolute; visibility: hidden;"
         type="file"
-        accept="image/*"
+        arounded-full/*"
         :disabled="uploading"
         @change="uploadAvatar"
       >
-    </div>
+    </div> -->
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -8,18 +8,23 @@
       <!-- <p v-if="profiles">{{ profiles  }}</p> -->
       <ul v-if="profiles">
         <li v-for="profile in profiles" :key="profile['id']">
+          <Avatar :path="profile['avatar_url']"/>
           {{ profile['username'] }} <br> {{ profile['website'] }}
+          <!-- <UAvatar :src="'https://umjsqfwlhbsyfaqlyanw.supabase.co/storage/v1/object/sign/avatars/' + " alt="Avatar" size="xl" /> -->
         </li>
       </ul>
       <p v-else>
         Loading...
       </p>
+      {{ profiles }}
     </div>
   </UContainer>
+
 </template>
 
 <script setup lang="ts">
 const client = useSupabaseClient()
+const baseUrl = 'https://umjsqfwlhbsyfaqlyanw.supabase.co/storage/v1/object/sign/avatars/'
 
 const { data: profiles } = await useAsyncData('profiles', async () => {
   const { data } = await client.from('profiles').select('*')
