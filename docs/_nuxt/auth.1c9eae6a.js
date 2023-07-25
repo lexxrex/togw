ee5a2501fcc4e9d7ba4f1bd8027bc04eacef4e70
Lex <lexrex618@gmail.com>
2023-07-25 17:18:09 +0300

    Deploy

--- docs/_nuxt/auth.1c9eae6a.js
+++
@@ -1,1 +0,0 @@
-import{T as e,z as a,U as s}from"./entry.a64f2650.js";const r=e(()=>{if(!a().value)return s("/")});export{r as default};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
+    baseURL: '/togw/',
     head: {
         htmlAttrs: { dir: 'rtl', lang: 'he' },
     },
