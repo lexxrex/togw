fa6bbcb45a1cc5d3e68046c155fe59994ad6e1f8
Lex <lexrex618@gmail.com>
2023-07-25 21:29:59 +0300

    Deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    // baseURL: '/togw/',
+    baseURL: '/togw/',
     head: {
         htmlAttrs: { dir: 'rtl', lang: 'he' },
     },
@@ -9,9 +9,9 @@ export default defineNuxtConfig({
   // devtools: { enabled: true },
   ssr: false,
   target: 'static',
-  router: {
-    base: '/togw/',
- },
+//   router: {
+//     base: '/togw/',
+//  },
   modules: [
     '@nuxtjs/supabase',
     '@nuxthq/ui',
