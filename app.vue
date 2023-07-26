40aa6a1cdb134996c619281114d24b3a4d55304d
Lex <lexrex618@gmail.com>
2023-07-26 09:12:03 +0300

    Deploy

--- app.vue
+++ app.vue
@@ -12,6 +12,29 @@
   </AppContainer>
 </template>
 
+<script setup lang="ts">
+useHead({
+  title: 'העבודה הגדולה האחת',
+  meta: [
+    { name: 'description', content: 'האתר הרשמי של קבוצת העבודה הגדולה האחת בטלגרם' },
+    { name: 'msapplication-TileColor', content: '#000000' },
+    { name: 'theme-color', content: '#ffffff' }
+  ],
+  link: [
+  { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+  { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+  { rel: 'icon', type: 'image/png', sizes: '16x16x', href: '/favicon-16x16.png' },
+  { rel: 'manifest', href: '/site.webmanifest' },
+  { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#000000' },
+  ],
+  bodyAttrs: {
+    // class: 'test'
+  },
+  // script: [ { innerHTML: 'console.log(\'Hello world\')' } ]
+})
+</script>
+
+
 <style lang="postcss">
 html {
   @apply scroll-smooth;
---
+++ docs/_nuxt/auth.0ee22fe8.js
@@ -0,0 +1,1 @@
+import{V as e,y as a,W as s}from"./entry.54e4fc14.js";const r=e(()=>{if(!a().value)return s("/")});export{r as default};
