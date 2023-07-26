7d1780ea8a790871979086dc4420a1c5df5ecee9
Lex <lexrex618@gmail.com>
2023-07-26 21:07:46 +0300

    Fixed favicons urls

--- app.vue
+++ app.vue
@@ -32,9 +32,10 @@ useHead({
     { property: 'twitter:image', content: 'https://lexxrex.github.io/togw/img/togw-og-black-on-white.jpg' },
   ],
   link: [
-    { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-    { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-    { rel: 'icon', type: 'image/png', sizes: '16x16x', href: '/favicon-16x16.png' },
+    { rel: 'apple-touch-icon', sizes: '180x180', href: './apple-touch-icon.png' },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', href: './favicon-32x32.png' },
+    { rel: 'icon', type: 'image/png', sizes: '16x16x', href: './favicon-16x16.png' },
+    { rel: 'icon', href: './favicon.ico' },
     { rel: 'manifest', href: '/site.webmanifest' },
     { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#000000' },
   ],
